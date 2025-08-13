1f5e890ea92770926ce3331ce3912a9dfe8db7ae
Tyrone Jose <alt.tyronejose@gmail.com>
2025-08-13 18:42:26 -0400

    chore: remove all existing code to start a new approach

--- backend/chat/routing.py
+++
@@ -1,8 +0,0 @@
-"""Routing for Chat App."""
-
-from django.urls import path
-from .consumers import ChatConsumer
-
-websocket_urlpatterns: list = [
-    path("ws/chat/<str:room_name>/", ChatConsumer.as_asgi()),
-]
