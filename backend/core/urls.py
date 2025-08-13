1a77a4e11b341e7ca3aa5e19d290b82c9a3aa3f0
Tyrone Jose <alt.tyronejose@gmail.com>
2025-08-13 18:49:06 -0400

    feat: add docker compose and core settings backend configs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,24 +1,15 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from django.urls.resolvers import URLResolver
+from django.urls import path, include
 
-urlpatterns: list[URLResolver] = [
+
+urlpatterns: list = [
     path("admin/", admin.site.urls),
+    path("api/auth/", include("apps.accounts.urls")),
+    path("api/chat/", include("apps.chat.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
